2b787f8a61c07f3c60a6da150af60348e5af928d
siaoyu <schobitsy@gmail.com>
2022-01-12 21:53:29 +0800

    Create attraction.html

---
+++ js/attraction.js
@@ -0,0 +1,33 @@
+// swiperjs
+(function() {
+  $(function() {
+      var $accordion, $wideScreen;
+      $accordion = $('#accordion').children('li');
+      $wideScreen = $(window).width() > 767;
+      if ($wideScreen) {
+          $accordion.on('mouseenter click', function(e) {
+              var $this;
+              e.stopPropagation();
+              $this = $(this);
+              if ($this.hasClass('out')) {
+                  $this.addClass('out');
+              } else {
+                  $this.addClass('out');
+                  $this.siblings().removeClass('out');
+              }
+          });
+      } else {
+          $accordion.on('touchstart touchend', function(e) {
+              var $this;
+              e.stopPropagation();
+              $this = $(this);
+              if ($this.hasClass('out')) {
+                  $this.addClass('out');
+              } else {
+                  $this.addClass('out');
+                  $this.siblings().removeClass('out');
+              }
+          });
+      }
+  });
+}.call(this));
