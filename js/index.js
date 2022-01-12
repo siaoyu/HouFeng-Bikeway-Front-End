3838dd174cfb5f56c56c24b8ac787195b4d94847
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2022-01-13 00:22:06 +0800

    navbar  定位

--- js/index.js
+++ js/index.js
@@ -66,17 +66,25 @@ let percent = [29, 31, 33, 35, 36, 37.5, 38, 38.2,
   17, 17.5, 18.5, 19.5, 21, 22, 23, 25, 28, 32, 36, 40, 44, 49, 54, 59, 64, 70, 76, 78, 80, 82, 84, 85, 86, 86.5, 87, 87.5, 88, 88.5, 89, 89.5, 90, 90.5, 91,
   91, 90.5, 90, 89.5, 89, 88.5, 88, 87, 85.5, 84, 82, 80, 77, 74, 70, 66, 61, 55, 49, 43, 36, 30, 24, 18, 15, 14, 13, 11.5, 10, 8.5, 7.5, 7, 6.5, 6, 6, 6,
   6, 6.5, 7, 7.5, 7.5, 8, 8, 8.5, 9.5, 11, 12.5, 13.5, 14.5, 15.5, 16.5, 18, 19.5, 21, 23, 25, 27, 29, 33, 35, 37, 38, 39, 42, 45, 48, 51, 54, 56, 58, 60, 62, 64, 66, 68, 69.5, 71, 72, 72.5, 73, 73.5, 74, 74.5, 75, 75, 75
-]
+];
+let navbar = document.querySelector('.navbar')
 let movingBike = document.querySelector('.bike-moving')
 window.addEventListener('scroll', function () {
   let scrollPositionY = window.pageYOffset;
   let addMarginTop = (scrollPositionY / window.innerWidth) * 100;
-  // console.log(addMarginTop);
+  console.log(addMarginTop);
 
   if (addMarginTop < 14) {
     movingBike.style.display = 'none'
   } else {
     movingBike.style.display = 'block'
+  }
+  if (addMarginTop >= 49.5) {
+    navbar.style.top = '0%'
+
+  } else {
+    navbar.style.top = ''
+
   }
   for (let i = 14; i <= 155; i++) {
     if (Math.floor(addMarginTop) == i) {
