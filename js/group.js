0a8b8ea573d216baeac4038c3fc25fafeebf19cf
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2022-01-13 11:04:41 +0800

    group js更新

--- js/group.js
+++ js/group.js
@@ -8,11 +8,11 @@ titles.forEach((title, index) => {
   title.addEventListener('click', function () {
     console.log(this);
     console.log(index);
-    if (index === 1) {
+    if (index === 0) {
       floatTitle.style.left = '1%'
       floatTitle.innerText = '在線行程'
       active(index);
-    } else if (index === 2) {
+    } else if (index === 1) {
       floatTitle.style.left = '34%'
       floatTitle.innerText = '建立行程'
       active(index);
@@ -28,11 +28,11 @@ function active(index) {
   teams.classList.add('inactive');
   teamForm.classList.add('inactive');
   photoWall.classList.add('inactive');
-  if (index === 1) {
+  if (index === 0) {
     teamWall.style.height = ''
     teams.classList.remove('inactive');
     photoWall.classList.remove('inactive');
-  } else if (index === 2) {
+  } else if (index === 1) {
     teamWall.style.height = '62.5vw'
     teamForm.classList.remove('inactive');
   };
